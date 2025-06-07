87c3322f6c636ce4a1d8aee96db9518b97d821f8
KimYuKyung <kyg0954@naver.com>
2025-06-07 21:27:55 +0900

    feat: 문제 설명 컴포넌트 UI

---
+++ front/src/pages/ProblemRoom/pages/Solve/components/Description/index.tsx
@@ -0,0 +1,10 @@
+export default function Description() {
+  return (
+    <section className="space-y-4">
+      <div>
+        <h3 className="font-semibold">문제 설명</h3>
+        <p>문제에 대한 설명임니다.</p>
+      </div>
+    </section>
+  );
+}
