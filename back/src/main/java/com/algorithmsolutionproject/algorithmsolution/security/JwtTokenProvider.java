60dec07bd3a141db8c4695be311b60a3e65571e8
KimYuKyung <kyg0954@naver.com>
2025-06-10 02:27:56 +0900

    feat: JwtTokenProvider 구현

---
+++ back/src/main/java/com/algorithmsolutionproject/algorithmsolution/security/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package com.algorithmsolutionproject.algorithmsolution.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Collections;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+    private final long EXPIRATION_TIME = 1000 * 60 * 300;
+    private final Key secretKey;
+
+    public JwtTokenProvider(@Value("${SECRET}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        String email = claims.getSubject();
+        int userId = claims.get("userId", Integer.class);
+
+        CustomUserPrincipal principal = new CustomUserPrincipal(userId, email);
+
+        return new UsernamePasswordAuthenticationToken(principal, null, Collections.emptyList());
+    }
+
+    public String generateAccessToken(String email, Integer userId) {
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("userId", userId);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
