1aa3c7abc95d4caa41b10566ea5962c1330edc2d
KimYuKyung <kyg0954@naver.com>
2025-06-10 02:42:19 +0900

    feat: 유저 ì ë¡로그인된 유저 정보 가져오는 CustomOAuth2UserService ì구현

---
+++ back/src/main/java/com/algorithmsolutionproject/algorithmsolution/service/CustomOAuth2UserService.java
@@ -0,0 +1,35 @@
+package com.algorithmsolutionproject.algorithmsolution.service;
+
+import com.algorithmsolutionproject.algorithmsolution.entity.User;
+import com.algorithmsolutionproject.algorithmsolution.repository.UserRepository;
+import com.algorithmsolutionproject.algorithmsolution.security.CustomOAuth2User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+
+        userRepository.findByEmail(email)
+                .orElseGet(() -> userRepository.save(
+                        User.builder()
+                                .email(email)
+                                .userName(name)
+                                .build()
+                ));
+
+        return new CustomOAuth2User(oAuth2User);
+    }
+}
