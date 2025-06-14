5587dbddda109ff4939f30b31378391d2353e618
KimYuKyung <kyg0954@naver.com>
2025-06-14 21:14:34 +0900

    feat: 방 참여자 조회 api 구현을 위해 RoomController 수정

--- back/src/main/java/com/algorithmsolutionproject/algorithmsolution/controller/RoomController.java
+++ back/src/main/java/com/algorithmsolutionproject/algorithmsolution/controller/RoomController.java
@@ -6,6 +6,7 @@ import com.algorithmsolutionproject.algorithmsolution.dto.room.CreateRoomRespons
 import com.algorithmsolutionproject.algorithmsolution.dto.room.ExecuteCodeAndStoreResultRequest;
 import com.algorithmsolutionproject.algorithmsolution.dto.room.GetAllRoomsResponse;
 import com.algorithmsolutionproject.algorithmsolution.dto.room.GetRoomDetailResponse;
+import com.algorithmsolutionproject.algorithmsolution.dto.room.GetRoomParticipantsResponse;
 import com.algorithmsolutionproject.algorithmsolution.dto.room.GetSolvedProblemResultResponse;
 import com.algorithmsolutionproject.algorithmsolution.dto.room.GetSubmissionsInRoomResponse;
 import com.algorithmsolutionproject.algorithmsolution.dto.room.SubmitCodeRequest;
@@ -64,6 +65,17 @@ public class RoomController {
 
         GetRoomDetailResponse response = roomService.getRoomDetail(roomId);
         return ResponseEntity.ok(ApiResponse.success("방을 성공적으로 조회했습니다.", response));
+    }
+
+    // 방 참여자 조회
+    @GetMapping("/{roomId}/participants")
+    public ResponseEntity<ApiResponse<GetRoomParticipantsResponse>> getRoomParticipants(@PathVariable("roomId") Integer roomId) {
+        if (roomId == null) {
+            throw new IllegalArgumentException("roomId는 필수입니다.");
+        }
+
+        GetRoomParticipantsResponse response = roomService.getRoomParticipants(roomId);
+        return ResponseEntity.ok(ApiResponse.success("방 참여자들을 성공적으로 조회했습니다.", response));
     }
 
     // 문제풀이 시작
