2e7bf404659a4f47f8bb1b0da36fdd57cd721b27
KimYuKyung <kyg0954@naver.com>
2025-06-19 19:03:44 +0900

    test: 테스트 코드 기본적인 테스트 코드로 수정

--- back/src/test/java/com/algorithmsolutionproject/algorithmsolution/AlgorithmsolutionApplicationTests.java
+++ back/src/test/java/com/algorithmsolutionproject/algorithmsolution/AlgorithmsolutionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AlgorithmsolutionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void justPass() {
+    }
 
 }
